ada8a8d9e11552e2d03e7ea70f70a80c27410466
rpshende <rpshende@DESKTOP-VK57T7M>
2020-07-04 23:59:13 -0400

    start fetch calls

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,21 @@
 const apiKey = '2PtiV3DK-QEq4aGfxIMEVs1DcYDrdoy-EZ53eURQb8fOsO3X9swaM70yTBi15Ih9E1nnDTKogIWjlVJPuJdP-vy_ohKsv0irqI7irmhHyn5jG7TL7pquja1qKmDVXnYx';
+
+const Yelp = {
+     search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization: `Bearer ${apiKey}`
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {}
+                });
+            }
+        })
+    }
+};
+
+export default Yelp;
