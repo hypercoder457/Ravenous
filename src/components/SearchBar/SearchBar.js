bb91f54e7c9ca5bf307a21aec3d4babca964c14e
rpshende <rpshende@DESKTOP-VK57T7M>
2020-07-04 22:32:56 -0400

    add new method handleSortByChange

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -27,6 +27,10 @@ class SearchBar extends React.Component {
         };
     };
 
+    handleSortByChange(sortByOption) {
+        this.setState({ sortBy: sortByOption });
+    }
+
     renderSortByOptions() {
         return Object.keys(this.sortByOptions).map(sortByOption => {
             const sortByOptionValue = this.sortByOptions[sortByOption];
